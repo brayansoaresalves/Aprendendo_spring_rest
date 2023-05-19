62763ebfb4c8fc5dc057d6de3681310cf5bc39f6
brayansoaresalves <brayan.iub10@gmail.com>
2023-05-18 22:02:02 -0300

    Trabalhando com mais exceções, tratando quando na requisição passamos valores que nao existe e valores invalidos

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,15 +5,17 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	MENSAGEM_ENCOMPREENSIVEL("/mensagem-encompreensivel", "Mensagem Encompreensivel"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade Não Encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade está em Uso"), 
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");    
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	PROPRIEDADE_INEXISTENTE("/propriedade-inexistente", "Propriedade não existe");    
 	
 	private String title;
 	private String uri;
 	
 	ProblemType(String path, String title){
-		this.uri = "https://algafood.com.br/" + path;
+		this.uri = "https://algafood.com.br" + path;
 		this.title = title;
 	}
 	

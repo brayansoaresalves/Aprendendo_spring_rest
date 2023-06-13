a1197a59b02cceafc2b72a5161915b87c8a3e619
brayansoaresalves <brayan.iub10@gmail.com>
2023-06-13 20:16:21 -0300

    Criando banco de teste criando novo arquivo de propriedade em java test e função de teste que verifica o corpo da requisição quando faz uma inserção

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,25 +1,57 @@
 package com.algaworks.algafood;
 
+import org.flywaydb.core.Flyway;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
 	
-	@Test
-	public void deveRetornarStatus200_quandoConsultarCozinha() {
-		
+	@Autowired
+	private Flyway flyway;
+	
+	@BeforeEach
+	public void setup() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
 		
-		RestAssured.given().basePath("/cozinhas").port(port).accept(ContentType.JSON).when()
+		flyway.migrate();
+	}
+	
+	@Test
+	public void testRetornarStatus200_quandoConsultarCozinha() {
+		
+		RestAssured.given().accept(ContentType.JSON).when()
 		.get().then().statusCode(200);
+	}
+	
+	@Test
+	public void testconter4cozinhas_quandoConsultarCozinha() {
+		
+		RestAssured.given().accept(ContentType.JSON).when()
+		.get().then().body("", Matchers.hasSize(5))
+		.body("nome", Matchers.hasItems("INDIANA", "TAILANDEZA"));
+	}
+	
+	@Test
+	public void testRetornarStatus201_quandoCadastrarCozinha() {
+		RestAssured.given().body("{\"nome\": \"FRANCESA\"}")
+		.contentType(ContentType.JSON).accept(ContentType.JSON).when()
+		.post().then().statusCode(HttpStatus.CREATED.value());
 	}
 	
 	/*
